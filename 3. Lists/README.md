501343278a90248b9cb931b0ed142fc1c969f783
nkuhta <nicholas.kuhta@gmail.com>
2017-06-04 12:34:59 -0700

    word list code

--- 3. Lists/README.md
+++ 3. Lists/README.md
@@ -1,6 +1,6 @@
 #  Lists
 1.  **Lists.py** - Collections of characters assigned to list with index values [0,1,2,3...], len(), range(), multi-layered lists, for loops, concatenate, slicing, methods (ie. sort, append, extend), built-in functions (len,max,min,sum), list.split(), double splitting, pop/del/remove list elements.  
-
+2.  **word_list.py** - Ask user for input file (romeo.txt) and split each line into a list and create a sorted list of all unique words in the document.  
 
 
 #  Python For Everybody Course References
---
+++ 3. Lists/word_list.py
@@ -0,0 +1,25 @@
+##############################################################
+################         Word Lists          #################
+##############################################################
+
+#   We will populate this list with all words in given file
+word_list=list()
+#   Enter file name we want the word list of
+fname = input('filename? ')
+#   file handle
+fhand=open(fname)
+
+#  for each line in the file
+for line in fhand:
+    #   create a split list of words for each line.
+    lsp = line.split()
+    #   loop over each word in the list in the current line
+    for i in range(len(lsp)):
+        #   While the word in the split line word list is not in the global word list 
+        while lsp[i] not in word_list:
+            #   append each word not in word_list to the list
+            word_list.append(lsp[i])
+
+#   Sort the word list
+word_list.sort()
+print(word_list)
