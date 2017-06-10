9dfb5ded95d327f7b81d5061292da19dd4b7f865
nkuhta <nicholas.kuhta@gmail.com>
2017-06-10 13:02:54 -0700

    email dict()

--- 4. Dictionaries/email_dict.py
+++ 4. Dictionaries/email_dict.py
@@ -2,6 +2,7 @@
 ###########         Email Address Dictionary from file          ################
 ################################################################################
 
+#  Make the email dict() out of the given filename
 fname=input('input filename:')
 fhand=open(fname)
 email_dic=dict()
@@ -12,3 +13,14 @@ for line in fhand:
         email=line[1]
         email_dic[email]=email_dic.get(email,0)+1
 print('email dictionary =',email_dic)
+
+#   find the maximum email counts
+max_email_counts = 0
+max_email_key = 0
+
+for key in email_dic:
+    if email_dic[key] > max_email_counts:
+        max_email_counts = email_dic[key]
+        max_email_key = key
+print('\nmost frequent email address -  corresponding counts')
+print(max_email_key,max_email_counts)
